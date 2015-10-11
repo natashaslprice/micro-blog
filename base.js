e9965d667f90907f5bb218a8b205ae5a5ea8a940
natashaslprice <natashaslprice@gmail.com>
2015-10-11 14:51:29 -0700

    background image, date, posts appending

--- base.js
+++ base.js
@@ -2,6 +2,29 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-  // code in here
+	// find todays date and append to headerDate div
+	var currentDate = new Date();
+	// var currentDateShort = currentDate.val($.datepicker.formatDate('dd M yy', new Date()));
+	$("#headerDate").append(currentDate);
+
+	// find starting number of posts, append to count div
+	var countPosts = $(".list").children().size();
+	$("#count").append(countPosts + " posts");
+
+
+	// take #newPostInput and append to list
+	$(".btn").click(function(e) {
+		e.preventDefault();
+		
+		var newPostInput = $("#newPostInput").val();
+		var postDate = new Date();
+		$(".list").append('<p>' + newPostInput + postDate + '</p>' +'<hr>');
+
+		// increase post count and append to count div
+		countPosts = countPosts + 1;
+		$("#count").text(" ").append(countPosts + " posts");
+	
+	});
 
 });
+

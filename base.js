2bd5e6b331a0cbb338d4b3339f9976ec0281faa8
natashaslprice <natashaslprice@gmail.com>
2015-10-12 17:48:33 -0700

    comments working

--- base.js
+++ base.js
@@ -9,28 +9,37 @@ $(document).ready(function(){
 
 	$("#headerDate").append(currentDateTime + '<hr>' + currentDateDate);
 
-	// if something in local storage, add to list
+
+	// if posts or comments in local storage, add to list
 	if (localStorage.getItem('posts')) {
 		$(".list").html(localStorage.getItem('posts'));
 	}
+	if (localStorage.getItem('comments')) {
+		$(".newCommentList").html(localStorage.getItem('comments'));
+	}
+
 
 	// find starting number of posts, append to count div
-	var countPosts = $("p").size();
+	var countPosts = $(".newPost").size();
 	$("#count").append(countPosts + " posts");
+
 
 	// create delete button
 	var deleteButton = '<button type="button" class="deleteButton" class="btn btn-default btn-sm"> <span class="glyphicon glyphicon-trash" aria-hidden="true"></span></button>';
 
 
-	// take #newPostInput and prepend to list
+	// on submit 
 	$("#form").on('submit', function(e) {
 		e.preventDefault();
 		
+		// check if #newPostInput has content and prepend
 		if ($("#newPostInput").val().trim().length > 0) {
 			var newPostInput = $("#newPostInput").val();
 			var newPostDate = new Date();
 			var newPostDateShort = $.format.date(newPostDate, 'H:mm, ddd d MMM');
-			$(".list").prepend('<div class="newPost"><p data-toggle="tooltip" data-placement="right" data-trigger="hover" title="' + newPostDateShort + '">' + newPostInput + deleteButton + '</p></div>');
+			var comment = '<div class="input-group comment"><input type="text" class="commentInput form-control" placeholder="Add comment..."><span class="input-group-btn commentAdd"><button type="button" class="btn btn-default">Add!</button></span></div>';
+
+			$(".list").prepend('<div class="newPost"><p data-toggle="tooltip" data-placement="right" data-trigger="hover" title="' + newPostDateShort + '">' + newPostInput + deleteButton + '<hr>' + comment + '<p class="newCommentList"></p>' + '</p></div>');
 
 			// increase post count and append to count div
 			countPosts = countPosts + 1;
@@ -45,13 +54,36 @@ $(document).ready(function(){
 		}	
 	});
 
+
 	// delete post if trash button clicked, change number of posts in count div
 	$(".list").on("click", ".deleteButton", function(e) {
 		e.preventDefault();
-		$(this).parent("p").remove();	
+		$(this).closest("div").remove();	
 		countPosts = countPosts - 1;
 		$("#count").text(" ").append(countPosts + " posts");
 	});
+
+
+	// on click of add comment button
+	$(".list").on("click", ".commentAdd", function(e) {
+		e.preventDefault();
+
+		// check if .commentInput has content and prepend
+		if ($(".commentInput").val().trim().length > 0) {
+			var commentInput = $(this).parent().find(".commentInput").val();
+			var commentDate = new Date();
+			var commentDateShort = $.format.date(commentDate, 'H:mm, ddd d MMM');
+			$(this).parent().siblings("p.newCommentList").prepend(commentInput + ' - ' + commentDateShort + '<br>');
+		}
+
+		// clear .commentInput area once comment prepended to list
+		$(".commentInput").val("");
+
+		// add new comment to local storage
+		var comment = $(".newCommentList").html();
+		localStorage.setItem('comments', comment);
+	});
+
 
 	// delete all from local storage
 	$("#deleteAllButton").click(function(e) {
